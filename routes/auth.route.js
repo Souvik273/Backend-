636a65d9d343a42d61cfb4f1e9ac2b335304e69c
Souvik273 <souvikgoswami464@gmail.com>
2025-01-08 01:07:08 +0530

    routes for reset password and send reset password committed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const {register,logIn,logOut} = require('../controller/user.controller')
+const {register,logIn,logOut , sendVerifyOtp , verifyEmail,isUserAuthenticated, sendResetOtp, resetPassword} = require('../controller/auth.controller.js')
+const userAuth = require('../middleware/user.auth.js')
 
 const authRouter = express.Router()
 
 authRouter.post('/register',register)
 authRouter.post('/login',logIn)
 authRouter.post('/logout',logOut)
+authRouter.post('/send-verify-otp',userAuth,sendVerifyOtp)
+authRouter.post('/verify-email',userAuth,verifyEmail)
+authRouter.post('/is-auth',userAuth,isUserAuthenticated)
+authRouter.post('/send-reset-otp',sendResetOtp)
+authRouter.post('/reset-password',resetPassword)
 
 module.exports=authRouter
